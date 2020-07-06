bf3ee7f50a56e580c50932d295fca7e7c1c44f52
duolamogu <1786416028@qq.com>
2020-07-06 10:40:04 +0800

    补充6.22 学习如何制作24小游戏

---
+++ gameforlesson/src/ai/Engine.java
@@ -0,0 +1,13 @@
+package ai;
+
+public class Engine {
+    public static void main(String[] args){
+        solve24(1,2,3,4);
+    }
+
+    private static String solve24(int i, int i1, int i2, int i3) {
+        //TODO
+        return "1*2*3*4=24";
+    }
+
+}
