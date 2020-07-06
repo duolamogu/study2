1aab5e28f8e3bf712de8c6b33e2152f1f50582d2
duolamogu <1786416028@qq.com>
2020-07-06 13:44:10 +0800

    补充6.22

--- gameforlesson/src/ai/Engine.java
+++ gameforlesson/src/ai/Engine.java
@@ -11,3 +11,17 @@ public class Engine {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ gameforlesson/src/ai/RPolandExpression.java
@@ -0,0 +1,49 @@
+package ai;
+import java.util.Stack;
+public class RPolandExpression {
+    static final double ERROR=-1;
+    public static void main(String[] args){
+        double val=eval("1 2 + 3 4 + *");
+        System.out.println(val=21.0);
+    }
+
+    private static double eval(String s) {
+        Stack<Double> S=new Stack<>();
+        String[] exp=s.split("\\s+");
+        for(String tok:exp){
+            if(isNumber(tok)){
+                S.push(Double.parseDouble(tok));
+            }else{
+                if(S.isEmpty()){
+                    return ERROR;
+                }
+                double b=S.peek();
+                S.pop();
+                if(S.isEmpty()){
+                    return ERROR;
+                }
+                double a=S.peek();
+                S.pop();
+                double c=0;
+                switch(tok){
+                    case "+":c=a+b;break;
+                    case "*":c=a*b;break;
+                    //case "-":c=a-b;break;
+                    //case "/":c=a/b;break;
+                }
+                S.push(c);
+            }
+        }
+        return S.size()!=1?ERROR:S.peek();
+    }
+
+    private static boolean isNumber(String tok) {
+        try{
+            Double.parseDouble(tok);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+}
