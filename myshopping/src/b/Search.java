71f11ae238dede9aa69b96f88fa2ce104cda4e0d
duolamogu <1786416028@qq.com>
2020-07-10 16:16:33 +0800

    补充搜索java

---
+++ myshopping/src/b/Search.java
@@ -0,0 +1,32 @@
+package b;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class Search extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletRequest resp)throws ServletException,IOException{
+        doPost(req,resp);
+    }
+    protected void doPost(HttpServletRequest req,HttpServletRequest)throws ServletException,IOException{
+        String partname=req.getParameter("partname");//搜索内容
+        IApostService service=new ApostService();
+        List<Apost>aposts=service.findByPartName(partname);
+        req.setAttribute("at",aposts);
+        ServletResponse resp;
+        req.getRequestDispatcher("search.jsp").forward(req,resp);
+    }
+
+    private class IApostService {
+        public List<Apost> findByPartName(String partname) {
+        }
+    }
+
+    private class ApostService extends IApostService {
+    }
+
+    private class Apost {
+    }
+}
